a4c47fb2cbb5869a3d7924d954ebfaf069c4ba1a
mani_g123 <mani_g123@yahoo.com>
2020-11-18 12:49:21 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,60 @@
+
+resource "azurerm_resource_group" "web_server_rg" {
+  name     = var.web_server_rg
+  location = var.web_server_location
+}
+resource "azurerm_virtual_network" "web_server_vnet"{
+  name                = "${var.resource_prefix}-vnet"
+  location            = var.web_server_location
+  resource_group_name = azurerm_resource_group.web_server_rg.name
+  address_space       = [var.web_server_address_space]
+
+}
+
+resource "azurerm_subnet" "web_server_subnet"{
+  name                  = "${var.resource_prefix}-vnet"
+  resource_group_name   = azurerm_resource_group.web_server_rg.name
+  virtual_network_name  = azurerm_virtual_network.web_server_vnet.name
+  address_prefix        = var.web_server_address_prefix
+}
+resource "azurerm_network_interface" "web_server_nic"{
+name = "${var.web_server_name}-nic"
+location = var.web_server_location
+resource_group_name = azurerm_resource_group.web_server_rg.name
+  ip_configuration {
+    name                            = "${var.web_server_name}-ip"
+    subnet_id                       = azurerm_subnet.web_server_subnet.id
+    private_ip_address_allocation  = "dynamic"
+  }
+}
+
+resource "azurerm_public_ip" "web_server_public_ip"{
+  name                  = "${var.resource_prefix}-public-ip"
+  location              = var.web_server_location 
+  resource_group_name   = azurerm_resource_group.web_server_rg.name
+  allocation_method     = var.environment == "production" ? "Static" : "Dynamic"
+}
+
+resource "azurerm_network_security_group" "web_server_nsg"{
+  name                = "${var.resource_prefix}-nsg"
+  location            = var.web_server_location
+  resource_group_name = azurerm_resource_group.web_server_rg.name
+}
+
+resource "azurerm_network_security_rule" "web_server_nsg_rule_rdp"{
+  name                  = "RDP Inbound"
+  priority              =100
+  direction             ="Inbound"
+  access                ="Allow"
+  protocol              ="Tcp"
+  source_port_range     ="*"
+  destination_port_range="3389"
+  source_address_prefix ="*"
+  destination_address_prefix= "*"
+  resource_group_name= azurerm_resource_group.web_server_rg.name
+  network_security_group_name=azurerm_network_security_group.web_server_nsg.name
+}
+resource "azurerm_network_interface_security_group_association" "web_server_nsg_association" {
+network_security_group_id=azurerm_network_security_group.web_server_nsg.id
+network_interface_id=azurerm_network_interface.web_server_nic.id
+}
---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+  version = "2.2.0"
+  features {}
+
+subscription_id ="fcc17b76-ad6d-4235-a4ab-41b6ff9c499a"
+client_id       ="5d0f79c5-4aec-47ee-b2b0-4badd3dc7883"
+client_secret   ="3xmwjg~N7dj.MnNN7UBvtNYK0T01O3tQ0W"
+tenant_id       ="4fc9337e-a0c4-4126-a00a-506a7112a194"
+}
+
+
